d6b0c6c62f9505d52f5caf6c1a5c2268e7031334
Matyas Kollert <M.Kollert@student.tudelft.nl>
2023-12-01 23:24:27 +0100

    better part 2

--- 1/p.py
+++ 1/p.py
@@ -27,3 +27,34 @@ for line in inp:
     nums2.append(int(digits[0] + digits[-1]))
 
 print("Part 2: " , sum(nums2))
+
+
+nums3 = []
+d = {
+    "one": "on1e",
+    "two": "tw2o",
+    "three": "thr3ee",
+    "four": "fo4ur",
+    "five": "fi5ve",
+    "six": "si6x",
+    "seven": "se7ven",
+    "eight": "eig8ht",
+    "nine": "ni9ne"
+}
+
+
+for line in inp:
+    ll = line
+    for key, val in d.items():
+        ll = ll.replace(key, val)
+    digits = [] 
+    for l in ll:
+        if l.isdigit():
+            digits.append(l)
+    nums3.append(int(digits[0] + digits[-1]))
+
+print(sum(nums3))
+
+
+
+
