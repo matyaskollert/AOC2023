46820484102e6f60a47b3b7bd6189605f11980ae
Matyas Kollert <M.Kollert@student.tudelft.nl>
2023-12-11 10:33:17 +0100

    .

--- 11/p.py
+++ 11/p.py
@@ -2,15 +2,15 @@ import sys
 import numpy as np
 
 with open(sys.argv[1], "r") as f:
-    inp = np.array([list(x.strip()) for x in f.readlines()])
+    inp = [list(x.strip()) for x in f.readlines()]
 
-x_space = []
-y_space = []
+x_empty = []
+y_empty = []
 galaxies = []
 
 for i,l in enumerate(inp):
     if "#" not in l:
-        y_space.append(i)
+        y_empty.append(i)
     else:
         for j,ll in enumerate(l):
             if ll == "#":
@@ -18,7 +18,7 @@ for i,l in enumerate(inp):
 
 for i,l in enumerate(np.transpose(inp)):
     if "#" not in l:
-        x_space.append(i)
+        x_empty.append(i)
 
 
 def dist(a,b):
@@ -28,22 +28,19 @@ def dist(a,b):
     addY2 = 0
     dx = np.abs(a[1] - b[1])
     dy = np.abs(a[0] - b[0])
-    min_x = min(a[1],b[1])
-    max_x = max(a[1],b[1])
-    min_y = min(a[0],b[0])
-    max_y = max(a[0],b[0])
-    for s in x_space:
-        if min_x < s < max_x:
+    for col in x_empty:
+        if min(a[1],b[1]) < col < max(a[1],b[1]):
             addX1 += 1
-            addX2 += 1000000 - 1 
+            addX2 += 10**6 - 1 
 
-    for s in y_space:
-        if min_y < s < max_y:
+    for row in y_empty:
+        if min(a[0],b[0]) < row < max(a[0],b[0]):
             addY1 += 1
-            addY2 += 1000000 -1 
+            addY2 += 10**6 -1 
     
-    return [dy+addY1+dx+addX1, dy+addY2+dx+addX2]
-    
+    p1 = dy + addY1 + dx + addX1
+    p2 = dy + addY2 + dx + addX2
+    return p1, p2
 
 part1 = []
 part2 = [] 
@@ -55,7 +52,6 @@ for i in range(len(galaxies)):
         d1, d2 = dist(g1,g2)
         part1.append(d1)
         part2.append(d2)
-        
 
 part2_b = np.array(part2, dtype='int64')
 print("Part 1", sum(part1))
